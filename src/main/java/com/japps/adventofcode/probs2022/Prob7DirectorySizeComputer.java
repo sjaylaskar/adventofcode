394487c457107268abf668bb708f77fe7cba2f73
sjaylaskar <0991@MeenuJoy>
2022-12-07 12:22:04 +0530

    [AoC2022] - Day 7.

--- src/main/java/com/japps/adventofcode/probs2022/Prob7DirectorySizeComputer.java
+++ src/main/java/com/japps/adventofcode/probs2022/Prob7DirectorySizeComputer.java
@@ -11,6 +11,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Stack;
+import java.util.stream.IntStream;
 
 import com.japps.adventofcode.util.AbstractSolvable;
 import com.japps.adventofcode.util.Loggable;
@@ -68,8 +69,8 @@ public final class Prob7DirectorySizeComputer extends AbstractSolvable implement
 	private void compute() throws IOException {
 		final Map<String, Long> dirSizes = buildDirPathSizes();
 
-		final long totalDirMemory = dirSizes.get("//");
-		final long freeMemory = totalDirMemory - 40000000L;
+		final long totalDirSizesMemory = dirSizes.get("//");
+		final long freeMemoryRequired = totalDirSizesMemory - 40000000L;
 
 		long lessThan100000SizeDirMemorySum = 0L;
 		long smallestDeletableDirMemory = Long.MAX_VALUE;
@@ -78,7 +79,7 @@ public final class Prob7DirectorySizeComputer extends AbstractSolvable implement
 			if (entry.getValue() <= 100000) {
 				lessThan100000SizeDirMemorySum += entry.getValue();
 			}
-			if (entry.getValue() >= freeMemory) {
+			if (entry.getValue() >= freeMemoryRequired) {
 				smallestDeletableDirMemory = Math.min(smallestDeletableDirMemory, entry.getValue());
 			}
 		}
@@ -102,28 +103,71 @@ public final class Prob7DirectorySizeComputer extends AbstractSolvable implement
 
 		for (final String line : lines) {
 			final String[] commandInputs = line.split(" ");
-			if (commandInputs.length >= 2 && commandInputs[1].equals("cd")) {
-				if (commandInputs.length >= 3 && commandInputs[2].equals("..")) {
+			if (isCommandCd(commandInputs)) {
+				if (isCommandBack(commandInputs)) {
 					path.pop();
 				} else {
 					path.add(commandInputs[2]);
 				}
-			} else if (commandInputs.length >= 2 && commandInputs[1].equals("ls")) {
-				continue;
-			} else if (commandInputs.length >= 1 && commandInputs[0].equals("dir")) {
+			} else if (isCommandListOrDir(commandInputs)) {
 				continue;
 			} else {
-				final long dirSize = Long.valueOf(commandInputs[0]);
-				final StringBuilder dirPathBuilder = new StringBuilder();
-				for (int i = 0; i < path.size(); i++) {
-					dirPathBuilder.append("/").append(path.get(i));
-					dirSizes.putIfAbsent(dirPathBuilder.toString(), 0L);
-					dirSizes.put(dirPathBuilder.toString(), dirSizes.get(dirPathBuilder.toString()) + dirSize);
-				}
+				buildDirPathSizes(dirSizes, path, commandInputs);
 			}
 		}
 
 		info("dirSizes: " + dirSizes);
 		return dirSizes;
+	}
+
+
+	/**
+	 * Checks if is command back.
+	 *
+	 * @param commandInputs the command inputs
+	 * @return true, if is command back
+	 */
+	private boolean isCommandBack(final String[] commandInputs) {
+		return commandInputs.length >= 3 && commandInputs[2].equals("..");
+	}
+
+
+	/**
+	 * Checks if is command cd.
+	 *
+	 * @param commandInputs the command inputs
+	 * @return true, if is command cd
+	 */
+	private boolean isCommandCd(final String[] commandInputs) {
+		return commandInputs.length >= 2 && commandInputs[1].equals("cd");
+	}
+
+	/**
+	 * Checks if is command list or dir.
+	 *
+	 * @param commandInputs the command inputs
+	 * @return true, if is command list or dir
+	 */
+	private boolean isCommandListOrDir(final String[] commandInputs) {
+		return commandInputs.length >= 2 && commandInputs[1].equals("ls")
+				   || commandInputs.length >= 1 && commandInputs[0].equals("dir");
+	}
+
+	/**
+	 * Builds the dir path sizes.
+	 *
+	 * @param dirSizes the dir sizes
+	 * @param path the path
+	 * @param commandInputs the command inputs
+	 */
+	private void buildDirPathSizes(final Map<String, Long> dirSizes, final Stack<String> path,
+			final String[] commandInputs) {
+		final long dirSize = Long.valueOf(commandInputs[0]);
+		final StringBuilder dirPathBuilder = new StringBuilder();
+		IntStream.range(0, path.size()).forEach(index -> {
+			dirPathBuilder.append("/").append(path.get(index));
+			dirSizes.putIfAbsent(dirPathBuilder.toString(), 0L);
+			dirSizes.put(dirPathBuilder.toString(), dirSizes.get(dirPathBuilder.toString()) + dirSize);
+		});
 	}
 }
