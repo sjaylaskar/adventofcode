6dad534ac0306a75e74c7214c158cbe510c7db1b
sjaylaskar <0991@MeenuJoy>
2023-03-27 14:03:13 +0530

    Add: EnglishAlphabetAnagramChecker.

---
+++ src/main/java/com/japps/adventofcode/util/HelloWorld.java
@@ -0,0 +1,28 @@
+/*
+ * Id: HeloWorld.java 27-Mar-2023 SubhajoyLaskar
+ * Copyright (©) 2023 Subhajoy Laskar
+ * https://www.linkedin.com/in/subhajoylaskar
+ */
+package com.japps.adventofcode.util;
+
+/**
+ * The hello world.
+ *
+ * @author Subhajoy Laskar
+ * @version 1.0
+ */
+public class HelloWorld {
+
+	static {
+		System.out.println("Hello World!");
+	}
+
+	/**
+	 * The main method.
+	 *
+	 * @param args the arguments
+	 */
+	public static void main(final String ...args) {
+
+	}
+}
