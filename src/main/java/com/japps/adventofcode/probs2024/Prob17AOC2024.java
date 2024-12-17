41f576763e3957b5d03c949b643d3c8e68a232f0
Subhajoy Laskar <subhajoy.laskar@gmail.com>
2024-12-17 19:29:39 +0530

    AOC2024 - DAY17.

--- src/main/java/com/japps/adventofcode/probs2024/Prob17AOC2024.java
+++ src/main/java/com/japps/adventofcode/probs2024/Prob17AOC2024.java
@@ -6,12 +6,17 @@ package com.japps.adventofcode.probs2024;
 
 import com.japps.adventofcode.util.AbstractSolvable;
 import com.japps.adventofcode.util.Loggable;
+import org.apache.commons.collections4.CollectionUtils;
 
 import java.io.IOException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
 
 import static com.japps.adventofcode.util.ProblemSolverUtil.COMMA;
 import static com.japps.adventofcode.util.ProblemSolverUtil.commify;
+import static java.util.Optional.ofNullable;
 
 public final class Prob17AOC2024 extends AbstractSolvable implements Loggable {
 
@@ -36,77 +41,51 @@ public final class Prob17AOC2024 extends AbstractSolvable implements Loggable {
     }
 
     private static final String INPUT_INSTRUCTION_PREFIX = ": ";
-    private static final String REGISTER_A = "A";
-    private static final String REGISTER_B = "B";
-    private static final String REGISTER_C = "C";
-    private static final long MIN_RANGE = 35184372088832L;
-    private static final long MAX_VALUE = Long.MAX_VALUE;
-    private static final long MAX_RANGE = MAX_VALUE / 32767;
 
     private void compute() throws IOException {
 		List<String> lines = lines();
-        long registerA = Integer.parseInt(lines.getFirst().substring(lines.getFirst().indexOf(INPUT_INSTRUCTION_PREFIX) + 2));
-        long registerB = Integer.parseInt(lines.get(1).substring(lines.get(1).indexOf(INPUT_INSTRUCTION_PREFIX) + 2));
-        long registerC = Integer.parseInt(lines.get(2).substring(lines.get(2).indexOf(INPUT_INSTRUCTION_PREFIX) + 2));
+        long registerA = Long.parseLong(lines.getFirst().substring(lines.getFirst().indexOf(INPUT_INSTRUCTION_PREFIX) + 2));
+        long registerB = Long.parseLong(lines.get(1).substring(lines.get(1).indexOf(INPUT_INSTRUCTION_PREFIX) + 2));
+        long registerC = Long.parseLong(lines.get(2).substring(lines.get(2).indexOf(INPUT_INSTRUCTION_PREFIX) + 2));
         List<Long> program = Arrays.stream(lines.get(4).substring(lines.get(4).indexOf(INPUT_INSTRUCTION_PREFIX) + 2).split(COMMA)).map(Long::parseLong).toList();
-        println(computeProgram(registerA, registerB, registerC, program));
-        println(computeRegisterAValue(program));
+        println(commify(computeProgram(registerA, registerB, registerC, program)));
+        long registerAValue = computeRegisterAValue(program);
+        println("Output equals: " + Objects.equals(program, computeProgram(registerAValue, registerB, registerC, program)));
+        println("Register A value: " + registerAValue);
     }
 
-    private long computeRegisterAValue(List<Long> program) {
-        for (long i = MIN_RANGE; i <= MAX_RANGE; i++) {
-            String commifiedOutput = computeProgram(i, 0, 0, program);
-            println("i: " + i + ", output: " + commifiedOutput);
-            println("Length of output: " + commifiedOutput.length());
-            if (commify(program).equals(commifiedOutput)) {
-                return i;
-            }
-        }
-        return -1;
-    }
-
-    private String computeProgram(long registerA, long registerB, long registerC, List<Long> program) {
-        Map<String, Long> registerMap = new HashMap<>();
-        registerMap.put(REGISTER_A, registerA);
-        registerMap.put(REGISTER_B, registerB);
-        registerMap.put(REGISTER_C, registerC);
-        List<Long> output = executeProgram(program, registerMap);
-        return commify(output);
-    }
-
-    private List<Long> executeProgram(List<Long> program, Map<String, Long> registerMap) {
+    private List<Long> computeProgram(long a, long b, long c, List<Long> program) {
         List<Long> output = new ArrayList<>();
-        int instructionPointer = 0;
-        while (instructionPointer < program.size()) {
+        for(int instructionPointer = 0; instructionPointer < program.size();) {
             long opcode = program.get(instructionPointer);
             long operand = program.get(instructionPointer + 1);
             switch ((int) opcode) {
-                case 0: // adv
-                    registerMap.put(REGISTER_A, advValue(registerMap, operand));
+                case 0:
+                    a >>= findActualOperand(a, b, c, operand);
                     break;
-                case 1: // bxl
-                    registerMap.put(REGISTER_B, registerMap.get(REGISTER_B) ^ operand);
+                case 1:
+                    b ^= operand;
                     break;
-                case 2: // bst
-                    registerMap.put(REGISTER_B, comboOpMod8(registerMap, operand));
+                case 2:
+                    b = actualOperandMod8(a, b, c, operand);
                     break;
-                case 3: // jnz
-                    if (registerMap.get(REGISTER_A) != 0) {
+                case 3:
+                    if (a != 0) {
                         instructionPointer = (int) operand;
                         continue;
                     }
                     break;
-                case 4: // bxc
-                    registerMap.put(REGISTER_B, registerMap.get(REGISTER_B) ^ registerMap.get(REGISTER_C));
+                case 4:
+                    b ^= c;
                     break;
-                case 5: // out
-                    output.add(comboOpMod8(registerMap, operand));
+                case 5:
+                    output.add(actualOperandMod8(a, b, c, operand));
                     break;
-                case 6: // bdv
-                    registerMap.put(REGISTER_B, advValue(registerMap, operand));
+                case 6:
+                    b = a >> findActualOperand(a, b, c, operand);
                     break;
-                case 7: // cdv
-                    registerMap.put(REGISTER_C, advValue(registerMap, operand));
+                case 7:
+                    c = a >> findActualOperand(a, b, c, operand);
                     break;
                 default:
                     throw new IllegalArgumentException("Invalid opcode: " + opcode);
@@ -116,21 +95,72 @@ public final class Prob17AOC2024 extends AbstractSolvable implements Loggable {
         return output;
     }
 
-    private long comboOpMod8(Map<String, Long> registerMap, long operand) {
-        return findActualOperand(registerMap, operand) % 8;
+    private static long computeRegisterAValue(List<Long> program) {
+        List<Long> outputRegisterAValueList = computeRegisterAValue(program, program, 0);
+        return (CollectionUtils.isNotEmpty(outputRegisterAValueList)) ? outputRegisterAValueList.getFirst() : -1;
     }
 
-    private long advValue(Map<String, Long> registerMap, long operand) {
-        return registerMap.get(REGISTER_A) / (long) (Math.pow(2, findActualOperand(registerMap, operand)));
+    private static List<Long> computeRegisterAValue(List<Long> program, List<Long> outputMatcher, long registerAValue) {
+        if (CollectionUtils.isEmpty(outputMatcher)) {
+            return new ArrayList<>();
+        }
+        for (long extender = 0; extender <= 7; extender++) {
+            long a = (registerAValue << 3) | extender;
+            long b = 0;
+            long c = 0;
+            long outputValue = -1;
+            for (int instructionPointer = 0; instructionPointer < program.size() - 2; instructionPointer += 2) {
+                long opcode = program.get(instructionPointer);
+                long operand = program.get(instructionPointer + 1);
+                switch ((int) opcode) {
+                    case 0:
+                        break;
+                    case 1:
+                        b ^= operand;
+                        break;
+                    case 2:
+                        b = actualOperandMod8(a, b, c, operand);
+                        break;
+                    case 4:
+                        b ^= c;
+                        break;
+                    case 5:
+                        outputValue = actualOperandMod8(a, b, c, operand);
+                        break;
+                    case 7:
+                        c = a >> findActualOperand(a, b, c, operand);
+                        break;
+                    default:
+                        throw new IllegalArgumentException("Invalid opcode: " + opcode);
+                }
+                List<Long> outputValueList = outputValueList(program, outputMatcher, outputValue, a);
+                if (outputValueList != null) {
+                    return outputValueList;
+                }
+            }
+        }
+        return null;
     }
 
-    private long findActualOperand(Map<String, Long> registerMap, long operand) {
+    private static List<Long> outputValueList(List<Long> program, List<Long> outputMatcher, long outputValue, long a) {
+        if (outputValue != -1 && outputMatcher.getLast().equals(outputValue)) {
+            List<Long> outputValueList = computeRegisterAValue(program, outputMatcher.subList(0, outputMatcher.size() - 1), a);
+            ofNullable(outputValueList).ifPresent(list -> list.add(a));
+            return outputValueList;
+        }
+        return null;
+    }
+
+    private static long findActualOperand(long a, long b, long c, long operand) {
         return switch ((int) operand) {
-            case 0, 1, 2, 3 -> operand;
-            case 4 -> registerMap.get(REGISTER_A);
-            case 5 -> registerMap.get(REGISTER_B);
-            case 6 -> registerMap.get(REGISTER_C);
-            default -> -1;
+            case 4 -> a;
+            case 5 -> b;
+            case 6 -> c;
+            default -> operand;
         };
+    }
+
+    private static long actualOperandMod8(long a, long b, long c, long operand) {
+        return findActualOperand(a, b, c, operand) % 8;
     }
 }
