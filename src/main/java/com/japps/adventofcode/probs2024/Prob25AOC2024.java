d9e834c8902502585acbc3887e75a7555137b4b8
Subhajoy Laskar <subhajoy.laskar@gmail.com>
2024-12-25 14:12:49 +0530

    AOC2024 - DAY25.

--- src/main/java/com/japps/adventofcode/probs2024/Prob25AOC2024.java
+++ src/main/java/com/japps/adventofcode/probs2024/Prob25AOC2024.java
@@ -8,59 +8,60 @@ import java.io.*;
 import java.util.*;
 import java.util.stream.*;
 
+import org.apache.commons.lang3.*;
+
 import com.japps.adventofcode.util.*;
 
 public final class Prob25AOC2024 extends AbstractSolvable implements Loggable {
 
-    private static final Prob25AOC2024 INSTANCE = instance();
+	private static final Prob25AOC2024 INSTANCE = instance();
 
-    private Prob25AOC2024() {
+	private Prob25AOC2024() {
 
-    }
+	}
 
-    private static Prob25AOC2024 instance() {
+	private static Prob25AOC2024 instance() {
 
-        return new Prob25AOC2024();
-    }
+		return new Prob25AOC2024();
+	}
 
-    public static void main(final String[] args) {
+	public static void main(final String[] args) {
 
-        try {
-            INSTANCE.compute();
-        } catch (final IOException exception) {
-            INSTANCE.error(exception.getLocalizedMessage());
-        }
-    }
+		try {
+			INSTANCE.compute();
+		} catch (final IOException exception) {
+			INSTANCE.error(exception.getLocalizedMessage());
+		}
+	}
 
-    private static final String LOCK_TOP_PATTERN = "#####";
-    private static final String KEY_TOP_PATTERN = ".....";
-    private static final char FILLED_PATTERN = '#';
+	private static final String LOCK_TOP_PATTERN = "#####";
+	private static final String KEY_TOP_PATTERN = ".....";
+	private static final char FILLED_PATTERN = '#';
 
-    private void compute() throws IOException {
+	private void compute() throws IOException {
 		List<String> lines = lines();
-        List<String> locks = new ArrayList<>();
-        List<String> keys = new ArrayList<>();
-		for (int i = 0; i < lines.size(); i+=8) {
-            List<String> lockOrKey = lines.subList(i, i + 7);
-            if (LOCK_TOP_PATTERN.equals(lockOrKey.getFirst())) {
-                locks.add(buildCombination(lockOrKey));
-            } else if (KEY_TOP_PATTERN.equals(lockOrKey.getFirst())) {
-                keys.add(buildCombination(lockOrKey));
-            }
-        }
+		List<String> locks = new ArrayList<>();
+		List<String> keys = new ArrayList<>();
+		fillLocksAndKeys(lines, locks, keys);
+		println(locks.stream().mapToLong(lock -> keys.stream().filter(key -> isFitPair(lock, key)).count()).sum());
+	}
 
-        println(locks.stream().mapToLong(lock -> keys.stream().filter(key -> isFitPair(lock, key)).count()).sum());
-    }
+	private void fillLocksAndKeys(List<String> lines, List<String> locks, List<String> keys) {
+		for (int i = 0; i < lines.size(); i += 8) {
+			List<String> lockOrKey = lines.subList(i, i + 7);
+			if (LOCK_TOP_PATTERN.equals(lockOrKey.getFirst())) {
+				locks.add(buildCombination(lockOrKey));
+			} else if (KEY_TOP_PATTERN.equals(lockOrKey.getFirst())) {
+				keys.add(buildCombination(lockOrKey));
+			}
+		}
+	}
 
-    private static boolean isFitPair(String lock, String key) {
-        return IntStream.range(0, key.length()).allMatch(columnIndex -> Integer.parseInt(String.valueOf(lock.charAt(columnIndex))) + Integer.parseInt(String.valueOf(key.charAt(columnIndex))) <= key.length());
-    }
+	private static boolean isFitPair(String lock, String key) {
+		return IntStream.range(0, key.length()).allMatch(columnIndex -> Integer.parseInt(String.valueOf(lock.charAt(columnIndex))) + Integer.parseInt(String.valueOf(key.charAt(columnIndex))) <= key.length());
+	}
 
-    private String buildCombination(List<String> lockOrKey) {
-        StringBuilder lockOrKeyPatternBuilder = new StringBuilder();
-        IntStream.range(0, lockOrKey.getFirst().length())
-                .forEach(columnIndex -> lockOrKeyPatternBuilder.append(
-                        lockOrKey.stream().filter(lockOrKeyLine -> FILLED_PATTERN == lockOrKeyLine.charAt(columnIndex)).count() - 1));
-        return lockOrKeyPatternBuilder.toString();
-    }
+	private String buildCombination(List<String> lockOrKey) {
+		return IntStream.range(0, lockOrKey.getFirst().length()).mapToObj(columnIndex -> String.valueOf(lockOrKey.stream().filter(lockOrKeyLine -> FILLED_PATTERN == lockOrKeyLine.charAt(columnIndex)).count() - 1)).collect(Collectors.joining(StringUtils.EMPTY));
+	}
 }
